8a93ef1c401565a8ca2d79478a365370fc6a38d2
tomaszosk <t.oskroba@icloud.com>
2019-10-07 22:36:52 +0200

    All UIControlls added to the storyboard

--- Bull's Eye/ViewController.swift
+++ Bull's Eye/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, world", message: "This is my first app", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
