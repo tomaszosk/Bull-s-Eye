79e2c97b75e5f608b112f6a37f7b4c25f469b1e4
tomaszosk <t.oskroba@icloud.com>
2019-10-31 19:05:41 +0100

    AboutViewController added

---
+++ Bull's Eye/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  Bull's Eye
+//
+//  Created by Tomasz Oskroba on 10/31/19.
+//  Copyright © 2019 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+//    @IBAction func close() {
+//        dismiss(animated: true, completion: nil)
+//    }
+    
+    @IBAction func closeButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
